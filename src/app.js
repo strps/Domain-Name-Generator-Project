3ce48b2e1dfae9edbf5be5dd94d12471831e6545
strps <csrstrps@gmail.com>
2022-11-19 21:18:50 +0000

    Adding domain name generator functionality

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronouns = ["the", "our"];
+  var adjs = ["great", "big"];
+  var nouns = ["jogger", "racoon"];
+  var extensions = [".com", ".biz", ".inc"];
+
+  var listgroup = document.getElementById("domain-name-list");
+
+  var liList = "";
+
+  for (let pronoun = 0; pronoun < pronouns.length; pronoun++) {
+    for (let adj = 0; adj < adjs.length; adj++) {
+      for (let noun = 0; noun < nouns.length; noun++) {
+        for (let extension = 0; extension < extensions.length; extension++) {
+          let domainName =
+            pronouns[pronoun] + adjs[adj] + nouns[noun] + extensions[extension];
+          liList += '<li class="list-group-item">' + domainName + "</li>";
+        }
+      }
+    }
+  }
+
+  listgroup.innerHTML = liList;
 };
